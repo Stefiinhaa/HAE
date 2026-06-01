72ef47482bb17373c4033a6eb4cd44e72a4e2462
Stefani <stefanisantos1212@gmail.com>
2026-06-01 17:46:40 -0300

    banco_novo

--- sistema_hae.sql
+++ sistema_hae.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 01/06/2026 às 03:25
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- Tempo de geração: 01-Jun-2026 às 22:45
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `relatorios_hae`
+-- Estrutura da tabela `relatorios_hae`
 --
 
 CREATE TABLE `relatorios_hae` (
@@ -38,22 +38,24 @@ CREATE TABLE `relatorios_hae` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `relatorios_hae`
+-- Extraindo dados da tabela `relatorios_hae`
 --
 
 INSERT INTO `relatorios_hae` (`id`, `solicitacao_id`, `mes_referencia`, `ano_referencia`, `acoes_realizadas`, `status`, `data_envio`) VALUES
 (1, 1, 5, 2026, 'Dia 06/11/2024 reunião do comitê organizador do evento a pedido do Fábio Dias foi reagendado, pois o presidente da ACIG Fábio Raniel não poderá participar.\r\n\r\nDia 11/11/2024 reunião do comitê organizador do evento a pedido do presidente da Acig, Fábio Raniel foi adiado.\r\n\r\nDia 19/11/2024 reunião com Wagner, empresa Fulltime que está apoiando e nos ajudando a trazer a Amazon para o evento. Discutimos possíveis eventos (Deep Racer e Hackathon) a serem inseridos na feira e a questão da Acig.\r\n\r\nDia 26/11/2024 reunião com Wilson e Fernando da Fulltime, onde a Amazon pediu ajustes no site e redes sociais para conseguir a aprovação da participação do evento. Será também agendada reunião com o Mauro que faz parte do conselho deliberativo da Acig para novos alinhamentos, na primeira semana de Dezembro.\r\n\r\nDia 28/11/2024 atualizado o site e as redes sociais as melhorias solicitadas pela Amazon para a aprovação de sua participação no evento.', 'Publicado', '2026-05-05 21:50:44'),
 (2, 2, 4, 2026, 'asdtfrytuylhj asrdtfygh asrdtjfgyhl etwayesrudtfygAESRYDTFYI AAAAAAAAAAAAA SDD223', 'Publicado', '2026-05-05 22:27:25'),
-(3, 2, 5, 2026, 'SADRDHTRJYKUL', 'Rascunho', '2026-05-05 22:22:22'),
+(3, 2, 5, 2026, 'maioooo maioo maioo', 'Publicado', '2026-06-01 19:00:37'),
 (4, 1, 4, 2026, 'AXZ\\ZCVX CDSA\\AX\\CXVBNXS\\ WFERD DF BGCVXCZSX AAAAAAAAAAAAA', 'Publicado', '2026-05-31 21:45:44'),
 (5, 4, 6, 2026, 'testandoooooo   testandoooooo testandoooooo testandoooooo vv testandoooooo', 'Publicado', '2026-06-01 00:44:50'),
 (6, 1, 6, 2026, 'eeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeee eeeeeeeeeee eeeeeeeeeeee', 'Publicado', '2026-06-01 00:44:37'),
-(7, 4, 5, 2026, 'rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr', 'Publicado', '2026-06-01 01:22:13');
+(7, 4, 5, 2026, 'rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr', 'Publicado', '2026-06-01 01:22:13'),
+(8, 2, 6, 2026, 'rrrrrrrrrrrr rrrrrrrrrrrrrr rrrrrrrrrrrrr rrrrrrrr rrrr rrrrrrr', 'Rascunho', '2026-06-01 18:59:17'),
+(9, 4, 4, 2026, 'abril abril abril', 'Publicado', '2026-06-01 19:51:22');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `solicitacoes_hae`
+-- Estrutura da tabela `solicitacoes_hae`
 --
 
 CREATE TABLE `solicitacoes_hae` (
@@ -86,7 +88,7 @@ CREATE TABLE `solicitacoes_hae` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `solicitacoes_hae`
+-- Extraindo dados da tabela `solicitacoes_hae`
 --
 
 INSERT INTO `solicitacoes_hae` (`id`, `professor_id`, `semestre`, `quantidade_horas`, `horas_aprovadas`, `titulo_projeto`, `projeto_anterior`, `nome_projeto_anterior`, `objetivos_escola`, `horas_aula`, `horas_atividade`, `horas_especificas`, `total_semanal`, `total_mensal`, `categoria`, `justificativa`, `objetivo`, `metodologia`, `envolvidos`, `recursos_necessarios`, `detalhamento_recursos`, `cronograma`, `resultados_esperados`, `status_aprovacao`, `parecer_direcao`, `data_criacao`) VALUES
@@ -98,7 +100,7 @@ INSERT INTO `solicitacoes_hae` (`id`, `professor_id`, `semestre`, `quantidade_ho
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `usuarios`
+-- Estrutura da tabela `usuarios`
 --
 
 CREATE TABLE `usuarios` (
@@ -117,49 +119,51 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `usuarios`
+-- Extraindo dados da tabela `usuarios`
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `telefone_whatsapp`, `funcao`, `data_nascimento`, `data_admissao`, `tipo_contrato`, `formacao_academica`, `assinatura_path`, `senha`, `primeiro_acesso`) VALUES
 (1, 'Stefani Santos', 'stefanisantos1212@gmail.com', '(14) 99837-3207', 'Professor', '2006-11-12', '2026-02-02', 'Indeterminado', 'ADS', 'uploads/assinaturas/cb407ab5771005be2cb9e453adf39fb3.jpg', '1d8bbc4294d306e8ba3ec733b0b06180', 0),
 (2, 'Diretor Teste', 'diretor@fatec.sp.gov.br', '14999999999', 'Diretor', '1980-03-15', '2010-02-01', 'Indeterminado', 'Doutorado em Educação', 'uploads/assinaturas/4256e35039565917b300e919c260083b.jpg', '1d8bbc4294d306e8ba3ec733b0b06180', 0),
 (3, 'Teste professora', 'professora@gmail.com', '(14) 99837-3207', 'Professor', '2006-11-12', '2025-03-01', 'Indeterminado', 'ADS', 'uploads/assinaturas/dd9d1889e20ed1ca009b5a54459f8795.jpg', '1d8bbc4294d306e8ba3ec733b0b06180', 0),
-(4, 'professor_teste', 'professorteste@gmail.com', '(14) 99837-3207', 'Professor', '2006-11-12', '2014-04-04', 'Indeterminado', 'especialista em TI', 'uploads/assinaturas/4c7bd8b0a262942fec377e0cb3763944.jpg', '1d8bbc4294d306e8ba3ec733b0b06180', 0);
+(4, 'professor_teste', 'professorteste@gmail.com', '(14) 99837-3207', 'Professor', '2006-11-12', '2014-04-04', 'Indeterminado', 'especialista em TI', 'uploads/assinaturas/4c7bd8b0a262942fec377e0cb3763944.jpg', '1d8bbc4294d306e8ba3ec733b0b06180', 0),
+(5, 'eduarda professora', 'eduardamoraes@gmail.com', '(14) 99837-3207', 'Professor', NULL, NULL, NULL, NULL, NULL, 'c6a4cfeadb2503ba84e62d697b6591b9', 1);
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `relatorios_hae`
+-- Índices para tabela `relatorios_hae`
 --
 ALTER TABLE `relatorios_hae`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uk_projeto_mes_ano` (`solicitacao_id`,`mes_referencia`,`ano_referencia`),
   ADD KEY `solicitacao_id` (`solicitacao_id`);
 
 --
--- Índices de tabela `solicitacoes_hae`
+-- Índices para tabela `solicitacoes_hae`
 --
 ALTER TABLE `solicitacoes_hae`
   ADD PRIMARY KEY (`id`),
   ADD KEY `professor_id` (`professor_id`);
 
 --
--- Índices de tabela `usuarios`
+-- Índices para tabela `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `relatorios_hae`
 --
 ALTER TABLE `relatorios_hae`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de tabela `solicitacoes_hae`
@@ -171,20 +175,20 @@ ALTER TABLE `solicitacoes_hae`
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- Restrições para tabelas despejadas
+-- Restrições para despejos de tabelas
 --
 
 --
--- Restrições para tabelas `relatorios_hae`
+-- Limitadores para a tabela `relatorios_hae`
 --
 ALTER TABLE `relatorios_hae`
   ADD CONSTRAINT `relatorios_hae_ibfk_1` FOREIGN KEY (`solicitacao_id`) REFERENCES `solicitacoes_hae` (`id`);
 
 --
--- Restrições para tabelas `solicitacoes_hae`
+-- Limitadores para a tabela `solicitacoes_hae`
 --
 ALTER TABLE `solicitacoes_hae`
   ADD CONSTRAINT `solicitacoes_hae_ibfk_1` FOREIGN KEY (`professor_id`) REFERENCES `usuarios` (`id`);
